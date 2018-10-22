8ccbdce1023443d07e57b2a200f2b9a8579db00d
Kalpana Poosarla <joyci09@gmail.com>
2018-10-22 21:55:50 +0000

    added new mothods BestCodingBootcamp::CLI class

--- lib/bestcoding_bootcamp/cli.rb
+++ lib/bestcoding_bootcamp/cli.rb
@@ -2,25 +2,25 @@
 class BestcodingBootcamp::CLI
   
   def call
-    puts "Find out the Best Coding Bootcamps"
     list_bootcamps
     menu
   end
   
   def list_bootcamps
-   puts <<=DOC.gsub /^\s*/,' '
-    1. Le Wagon
-    2. Ironhack
-    3. App Academy
-    4. General Assembly
-    5. Bloc 
-    6. Thinkful
-    7. Flatiron School
-   DOC
+    puts "Find out the Best Coding Bootcamps"
+    puts <<-DOC.gsub /^\s*/,' '
+     1. Le Wagon
+     2. Ironhack
+     3. App Academy
+     4. General Assembly
+     5. Bloc 
+     6. Thinkful
+     7. Flatiron School
+    DOC
   end
   
   def menu
-    
+    puts "Enter number of the school you 'd like more info on?"
   end
   
   
