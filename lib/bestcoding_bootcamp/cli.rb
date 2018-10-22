0fcc11a25963a830791a97b3a18f9a880d575032
Kalpana Poosarla <joyci09@gmail.com>
2018-10-22 22:29:56 +0000

    developing the better interactive cli interface for the user

--- lib/bestcoding_bootcamp/cli.rb
+++ lib/bestcoding_bootcamp/cli.rb
@@ -4,6 +4,7 @@ class BestcodingBootcamp::CLI
   def call
     list_bootcamps
     menu
+    good_bye
   end
   
   def list_bootcamps
@@ -20,8 +21,24 @@ class BestcodingBootcamp::CLI
   end
   
   def menu
-    puts "Enter number of the school you 'd like more info on?"
+    
+    input = nil
+     while input != "exit"
+     puts "Enter number of the school you 'd like more info on? 1-7 or type list to find the list again type exit"
+      input = gets.strip.downcase
+      case input
+      when "1"
+        puts "More info on coding bootcamp 1"
+      when  "2"
+        puts "M0re info on coding bootcamp 2"
+      end
+   end
   end
+  
+  def good_bye
+    puts "Hope you found your school, All the best for tour coding"
+  end
+  
   
   
 end
